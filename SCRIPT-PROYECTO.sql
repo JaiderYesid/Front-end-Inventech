693b439841ed2e8e30b088ef3e7649d57715a92b
JHOSEPHERRERA <jhosepherrera5@gmail.com>
2024-09-10 13:11:23 -0500

    SCRIPT-PROYECTO.sql

--- SCRIPT-PROYECTO.sql
+++ SCRIPT-PROYECTO.sql
@@ -1,80 +1,107 @@
-DROP DATABASE IF EXISTS Inventario;
-CREATE DATABASE Inventario CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE Inventario;
+DROP DATABASE IF EXISTS proyecto;
 
-CREATE TABLE Proveedor(
-ID_Proveedor INT PRIMARY KEY AUTO_INCREMENT,
-Nombre VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Email VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Direccion VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Telefono INT
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE proyecto CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Categoria(
-ID_Categoria INT PRIMARY KEY AUTO_INCREMENT,
-Nombre VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Descripcion VARCHAR(1000)COLLATE utf8mb4_unicode_ci NOT NULL
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+use proyecto;
+ 
+create table Categoria (
+  ID INT PRIMARY KEY AUTO_INCREMENT,
+  NOMBRE VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  DESCRIPCIÓN VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Inventario(
-ID_Inventario INT PRIMARY KEY AUTO_INCREMENT,
-Registros VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Productos VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Fecha DATE,
-Stock INT
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+create table Modelo (
+ ID int primary key auto_increment,
+ NOMBRE VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+ TAMAÑO INT NOT NULL,
+ TOTAL INT NOT NULL,
+ ID_CATEGORIA INT NOT NULL,
+ FOREIGN KEY (ID_Categoria) references Categoria (ID)
+ ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ 
+create table Proveedor (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+EMAIL VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+DIRECCIÓN VARCHAR(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+TELEFONO INT NOT NULL
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Cliente(
-ID_Cliente INT PRIMARY KEY AUTO_INCREMENT,
-Nombre VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Email VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Direccion VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Telefono INT(20)
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+create table Funcionario (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+CARGO VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ EMAIL VARCHAR(50)COLLATE utf8mb4_unicode_ci NOT NULL,
+ CONTRASEÑA VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ REP_CONTRASEÑA VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ TELEFONO INT NOT NULL
+ ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ 
+create table Cliente (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+NOMBRE VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+EMAIL VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+DIRECCIÓN VARCHAR(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+TELEFONO INT NOT NULL,
+CONTRASEÑA VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+REP_CONTRASEÑA VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+ID_funcionario INT NOT NULL, 
+FOREIGN KEY (ID_funcionario) REFERENCES Funcionario (ID) 
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE TipoDeMovimiento(
-ID_Movimiento INT PRIMARY KEY AUTO_INCREMENT,
-Descripcion VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Inventario_ID INT NOT NULL,
-FOREIGN KEY (Inventario_ID) REFERENCES Inventario(ID_Inventario)
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+Create table Inventario (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+FECHA DATE,
+STOCK INT 
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Factura(
-ID_Factura INT PRIMARY KEY AUTO_INCREMENT,
-Fecha DATE,
-Valor_Total FLOAT,
-IVA INT,
-Inventario_ID INT NOT NULL,
-FOREIGN KEY (Inventario_ID) REFERENCES Inventario(ID_Inventario)
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+Create table Tipo_Movimiento (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+DESCRIPCIÓN VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+ID_inventario INT NOT NULL,
+FOREIGN KEY (ID_Inventario) REFERENCES Inventario (ID)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Venta(
-ID_Venta INT PRIMARY KEY AUTO_INCREMENT,
-Estado VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Fecha DATE,
-Total FLOAT,
-Cliente_ID INT NOT NULL,
-Factura_ID INT NOT NULL,
-FOREIGN KEY (Cliente_ID) REFERENCES Cliente(ID_Cliente),
-FOREIGN KEY (Factura_ID) REFERENCES Factura(ID_Factura)
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+Create Table Factura (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+FECHA DATE,
+VALOR_TOTAL INT NOT NULL,
+IVA INT NOT NULL,
+ID_Inventario INT NOT NULL,
+FOREIGN KEY (ID_Inventario) REFERENCES Inventario (ID)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE Producto(
-ID_Producto INT PRIMARY KEY AUTO_INCREMENT,
-Descripcion VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Precio FLOAT,
-Cantidad INT NOT NULL,
-Estado VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Nombre VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Stock INT,
-Tamaño FLOAT,
-Color VARCHAR(45)COLLATE utf8mb4_unicode_ci NOT NULL,
-Proveedor_ID INT NOT NULL,
-Categoria_ID INT NOT NULL,
-Inventario_ID INT NOT NULL,
-Venta_ID INT NOT NULL,
-FOREIGN KEY (Proveedor_ID) REFERENCES Proveedor(ID_Proveedor),
-FOREIGN KEY (Categoria_ID) REFERENCES Categoria(ID_Categoria),
-FOREIGN KEY (Inventario_ID) REFERENCES Inventario(ID_Inventario),
-FOREIGN KEY (Venta_ID) REFERENCES Venta(ID_Venta)
-)CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+Create table Venta (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+ESTADO VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+FECHA DATE NOT NULL,
+ID_Factura INT NOT NULL,
+ID_Cliente INT NOT NULL,
+FOREIGN KEY (ID_Factura) REFERENCES Factura (ID),
+FOREIGN KEY (ID_Cliente) REFERENCES Cliente (ID)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ create table FUNCIONARIO_has_VENTA (
+ ID_Funcionario INT NOT NULL,
+ Id_Venta INT NOT NULL,
+ FOREIGN KEY (ID_Funcionario) REFERENCES Funcionario (ID),
+ FOREIGN KEY (ID_Venta) REFERENCES Venta (ID)
+ );
+ 
+Create table Producto (
+ID INT PRIMARY KEY AUTO_INCREMENT,
+ESTADO VARCHAR(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+STOCK INT NOT NULL,
+COLOR VARCHAR (20) COLLATE  utf8mb4_unicode_ci NOT NULL,
+FECHA_CADUCIDAD DATE NOT NULL,
+ID_Proveedor INT NOT NULL,
+ID_Categoria INT NOT NULL,
+ID_Inventario INT NOT NULL,
+ID_Venta INT NOT NULL,
+ID_Modelo INT NOT NULL,
+foreign key (ID_Proveedor) REFERENCES Proveedor(ID),
+foreign key (ID_Categoria) REFERENCES Categoria(ID),
+foreign key (ID_Inventario) REFERENCES Inventario (ID),
+foreign key (ID_Venta) REFERENCES Venta(ID),
+Foreign key (ID_Modelo) REFERENCES Modelo(ID)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
